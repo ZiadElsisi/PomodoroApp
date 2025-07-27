a648ac1d7fbd4a363ea44dc44df40573eedd4440
ZiadElsisi <ziadsisi79@gmail.com>
2025-07-27 19:17:02 +0300

    pomodoro.py

--- pomodoro.py
+++ pomodoro.py
@@ -2,7 +2,8 @@ import time
 from tkinter import *
 import sys
 import os
-
+import pygame
+pygame.mixer.init()
 def resource_path(relative_path):
     """
     Get absolute path to resource, works for dev and for PyInstaller bundled .exe.
@@ -13,6 +14,8 @@ def resource_path(relative_path):
         base_path = os.path.abspath(".")
 
     return os.path.join(base_path, relative_path)
+
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -22,20 +25,38 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-secondes=0
+seconds=0
 count =False
 timer = None
 
+
+# ---------------------------- TIMER sound ------------------------------- #
+tickpath=resource_path("clock-ticking.wav")
+alarmpath = resource_path("the_best_alarm.mp3")
+ticksound=pygame.mixer.Sound(tickpath)
+alarm_Sound = pygame.mixer.Sound(alarmpath)
+def mute_sound() :
+    if ticksound.get_volume() ==0.0 :
+        ticksound.set_volume(1.0)
+        mute_button.config(text="mute",bg="#fc4903",activebackground="#c95b16")
+    else:
+        ticksound.set_volume(0.0)
+        mute_button.config(text="unmute", bg="#4CAF50",activebackground="#45a049")
+
+
 # ---------------------------- TIMER RESET ------------------------------- #
 def reset () :
-    global secondes,timer
+    global seconds,timer
+    alarm_Sound.set_volume(0.0)
+
     my_manager.pomo_counter=1
     check_right.config(text="✔" * int(my_manager.pomo_counter / 2), background=YELLOW, fg=GREEN, font=(FONT_NAME, 12))
     Time_label.config(text=f'timer', fg=GREEN)
+    my_manager.current_period = "Work"
 
     my_manager.minutes=WORK_MIN
-    secondes= 0
-    my_canvas.itemconfig(time_text,text =f'{my_manager.minutes:02d}:{secondes:02d}')
+    seconds= 0
+    my_canvas.itemconfig(time_text,text =f'{my_manager.minutes:02d}:{seconds:02d}')
     start_button.config(state="normal")
     if timer :
         my_screen.after_cancel(timer)
@@ -46,7 +67,7 @@ class MinutesManager :
         self.pomo_counter=1
         self.minutes = WORK_MIN
         self.current_period="Work"
-    def next_periodd (self)  :
+    def next_period (self)  :
         self.pomo_counter+=1
         if self.pomo_counter%8==0 :
             self.minutes=LONG_BREAK_MIN
@@ -61,39 +82,43 @@ class MinutesManager :
             self.minutes=WORK_MIN
             self.current_period="Work"
             Time_label.config(text=f'{self.current_period}',fg=GREEN)
-
+        alarm_Sound.set_volume(1.0)
+        alarm_Sound.play(maxtime=8000)
+        print("nnananana")
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
-def star_counter () :
+def start_counter () :
     global count
+    alarm_Sound.set_volume(0.0)
     count= True
     start_button.config(state="disabled")
     count_down()
 
 def count_down ():
-    global secondes,timer
+    global seconds,timer
     if my_manager.pomo_counter==1 :
         Time_label.config(text=f'{my_manager.current_period}', fg=GREEN)
 
-    if my_manager.minutes == 0 and secondes==0 :
-        my_manager.next_periodd()
+    if my_manager.minutes == 0 and seconds==0 :
+        my_manager.next_period()
         check_right.config(text="✔"*int(my_manager.pomo_counter/2),background=YELLOW,fg=GREEN,font=(FONT_NAME,12))
 
         my_screen.after_cancel(timer)
         start_button.config(state="normal")
-        my_canvas.itemconfig(time_text,text =f'{my_manager.minutes:02d}:{secondes:02d}')
+        my_canvas.itemconfig(time_text,text =f'{my_manager.minutes:02d}:{seconds:02d}')
 
         return
-    if secondes == 0 :
+    if seconds == 0 :
         my_manager.minutes-=1
-        secondes = 60
-
-    secondes-=1
+        seconds = 60
+    if not pygame.mixer.get_busy():
+            ticksound.play(maxtime=100)
+    seconds-=1
     minutes = my_manager.minutes
 
-    my_canvas.itemconfig(time_text,text =f'{minutes:02d}:{secondes:02d}')
+    my_canvas.itemconfig(time_text,text =f'{minutes:02d}:{seconds:02d}')
 
 
 
@@ -115,18 +140,19 @@ tomato = PhotoImage(file=tomato_img_path)
 my_canvas.create_image(104,112,image=tomato)
 my_canvas.grid(row=1,column=1)
 time_text =my_canvas.create_text(112,130,text=f'25:00',fill="white",font=(FONT_NAME,28,"bold"))
-start_button = Button( text="Start",
-    command=star_counter,
-    bg="#4CAF50",        # background color
-    fg="white",          # text color
-    activebackground="#45a049",
-    font=("Arial", 12, "bold"),
-    relief="raised",     # border style
-    bd=4,                # border width
-    padx=10, pady=5    )
+start_button = Button( text="▶ Start",
+    command=start_counter,
+                       bg="#4CAF50",  # background color
+                       fg="white",  # text color
+                       activebackground="#45a049",
+                       font=("Arial", 12, "bold"),
+                       relief="raised",  # border style
+                       bd=4,  # border width
+                       padx=10, pady=5
+                       )
 start_button.grid(column=0,row=2)
 
-Reset_button = Button( text="Reset",
+Reset_button = Button( text="⟳ Reset",
     command=reset,
     bg="#4CAF50",        # background color
     fg="white",          # text color
@@ -137,15 +163,22 @@ Reset_button = Button( text="Reset",
     padx=10, pady=5    )
 Reset_button.grid(column=3,row=2)
 
+mute_button = Button( text="mute", width=8,
+    command=mute_sound,
+    bg="#fc4903",        # background color
+    fg="white",          # text color
+    activebackground="#c95b16",
+    font=("Arial", 9, "bold"),
+    # relief="raised",     # border style
+    # bd=4,                # border width
+    padx=0, pady=5    )
+mute_button.place(x=-60,y=-25)
+
 
 check_right =Label(text="",background=YELLOW,fg=GREEN,font=(FONT_NAME,12))
 check_right.grid(column=1,row=3)
 
 
 
-
-
-
 my_screen.mainloop()
-
 
